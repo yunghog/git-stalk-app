c5bde8626a5e1887f9eccad4b39f83bf2d8ab595
Samartha <samarthahm@gmail.com>
2022-12-14 10:04:26 +0530

    init 2.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { Storage } from '@ionic/storage';
-
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent,SidebarComponent],
     imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
     providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, Storage],
     bootstrap: [AppComponent]
