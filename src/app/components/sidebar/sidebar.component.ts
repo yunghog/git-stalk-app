41884f3dc30426641f8a2dc39725975c901fd7eb
Samartha <samarthahm@gmail.com>
2022-12-14 10:04:44 +0530

    create sidebar component

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { Storage } from '@ionic/storage';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss'],
+})
+export class SidebarComponent implements OnInit {
+  constructor(private menu: MenuController, private storage: Storage, private router: Router) {}
+
+  ngOnInit() {}
+  openFirst() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+  }
+  clear() {
+    this.storage.set('curUser', null);
+    this.router.navigateByUrl('/login');
+  }
+  closeMenu() {
+    this.menu.close('first');
+  }
+}
