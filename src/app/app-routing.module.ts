a72afbf7c1d726b67217039f616f00e8debf9e06
Samartha <samarthahm@gmail.com>
2022-12-15 09:43:35 +0530

    discontinue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,59 +2,13 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
-const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'splash',
-    pathMatch: 'full'
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'user-profile',
-    loadChildren: () => import('./user-profile/user-profile.module').then( m => m.UserProfilePageModule)
-  },
-  {
-    path: 'user-profile/:username',
-    loadChildren: () => import('./user-profile/user-profile.module').then( m => m.UserProfilePageModule)
-  },
-  {
-    path: 'watch',
-    loadChildren: () => import('./watch/watch.module').then( m => m.WatchPageModule)
-  },
-  {
-    path: 'signup',
-    loadChildren: () => import('./signup/signup.module').then( m => m.SignupPageModule)
-  },
-  {
-    path: 'repositories/:username/:repository',
-    loadChildren: () => import('./repositories/repositories.module').then( m => m.RepositoriesPageModule)
-  },
-  {
-    path: 'saved-repo',
-    loadChildren: () => import('./saved-repo/saved-repo.module').then( m => m.SavedRepoPageModule)
-  },
-  {
-    path: 'splash',
-    loadChildren: () => import('./splash/splash.module').then( m => m.SplashPageModule)
-  },
-  {
-    path: 'new-update',
-    loadChildren: () => import('./new-update/new-update.module').then( m => m.NewUpdatePageModule)
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
     HttpClientModule,
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
